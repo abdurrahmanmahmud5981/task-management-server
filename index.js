955d56c71df0246f6794b44785214ce129a011d3
Abdur Rahman Mahmud <ar7543078@gmail.com>
2025-02-20 21:22:11 +0600

    server setup completed

---
+++ index.js
@@ -0,0 +1,25 @@
+require('dotenv').config()
+const express = require('express')
+const cors = require('cors')
+const cookieParser = require('cookie-parser')
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb')
+const jwt = require('jsonwebtoken')
+
+
+const morgan = require('morgan')
+
+const port = process.env.PORT || 5000
+const app = express()
+
+
+// middleware
+const corsOptions = {
+    origin: ['http://localhost:5173', 'http://localhost:4173'],
+    credentials: true,
+    optionSuccessStatus: 200,
+}
+app.use(cors(corsOptions))
+
+app.use(express.json())
+app.use(cookieParser())
+app.use(morgan('dev'))
